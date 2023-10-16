ca6af595bf9db46902831724e26491cbca421d48
Joeliel <joelielfuentes12@gmail.com>
2023-10-16 10:25:52 -0400

    Adding work code

--- variables_if_else_while/0-positive_or)negative.c
+++ variables_if_else_while/0-positive_or)negative.c
@@ -2,19 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     int n;
-    srand((unsigned int)time(NULL));
+
+    srand(time(0));
     n = rand() - RAND_MAX / 2;
 
     printf("El número es %d\n", n);
 
-    if (n > 0) {
+    if (n > 0)
+    {
         printf("es positivo\n");
-    } else if (n == 0) {
+    }
+    else if (n == 0)
+    {
         printf("es cero\n");
-    } else {
+    }
+    else
+    {
         printf("es negativo\n");
     }
 
-    return 0;
+    return (0);
+}
