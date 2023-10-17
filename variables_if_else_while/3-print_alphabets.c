c98d95bf059fed2ca673bc698fd6bbd44f6e1e68
Joeliel <joelielfuentes12@gmail.com>
2023-10-16 22:22:09 -0400

    Adding work code

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char letter;
+
+    letter = 'a';
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    letter = 'A';
+    while (letter <= 'Z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
